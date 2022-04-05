d2b81c3872f5cd2e6b51f69bef743914dfe2b3f1
dujthep-odds <dujthep@odds.team>
2022-04-05 22:35:02 +0700

    ccreate impl Dog and fn bark

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+pub struct Dog;
+pub struct Interpreter;
+
+impl Dog {
+    pub fn bark(&self) {
+        println!("baf");
+    }
+}
+
+impl Interpreter {
+    pub fn input() {
+
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+
+// use tutorial::Interpreter;
+use tutorial::Dog;
+
+fn main() {
+    let d = Dog;
+    d.bark()
+    
+}
+
+#[test]
+fn basic_test() {
+    // let mut i = Interpreter;
+    // assert_eq!(i.input("1 + 1"), Ok(Some(2.0)));
+}
