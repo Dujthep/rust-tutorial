e5fd0b398abba4d8b54b575174077f9f11babeef
dujthep-odds <dujthep@odds.team>
2022-04-07 00:33:51 +0700

    try option return.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,33 @@ impl Dog {
     }
 }
 
+pub struct Interpreter {
+    res: f32,
+    input: String,
+}
+
+impl std::fmt::Display for Interpreter {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "(response is : {}, input is : {})", self.res, self.input)
+    }
+}
+
+impl Interpreter {
+    pub fn new() -> Interpreter {
+        return Interpreter {
+            res: 1.0,
+            input: String::from("1.0 + 0.0"),
+        };
+    }
+
+    pub fn input(&mut self, _input: &str) -> Result<Option<f32>, String> {
+        if _input == "A" {
+            return Ok(Some(1.0));
+        }
+        return Err(_input.to_string())
+    }
+}
+
 pub enum FooBar {
     Foo,
     Bar,
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
+use tutorial::Interpreter;
 use tutorial::FooBar;
 use tutorial::Dog;
 
 fn main() {
     let dog = Dog;
     dog.bark();
-    
+
     let foobar: Option<FooBar> = Some (FooBar::Bar);
     match foobar {
         Some(FooBar::Foo) => { println!("Foo") }
         Some(FooBar::Bar) => { println!("Bar") }
         _ => { println!("None") }
     }
+    
+    let mut i = Interpreter::new();
+    println!("{}", i);
+    println!("{:?}", i.input("BA"));
 }
