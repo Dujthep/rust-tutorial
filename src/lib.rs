8709f424557a0bce6961d89701e4f849891a263b
dujthep-odds <dujthep@odds.team>
2022-04-08 00:42:03 +0700

    add fn ez basic arithmetic.

--- src/lib.rs
+++ src/lib.rs
@@ -7,29 +7,70 @@ impl Dog {
 }
 
 pub struct Interpreter {
-    res: f32,
-    input: String,
+    // res: f32,
+    // input: String,
 }
 
-impl std::fmt::Display for Interpreter {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "(response is : {}, input is : {})", self.res, self.input)
-    }
-}
+// impl std::fmt::Display for Interpreter {
+//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+//         write!(f, "(response is : {}, input is : {})", self.res, self.input)
+//     }
+// }
 
 impl Interpreter {
-    pub fn new() -> Interpreter {
-        return Interpreter {
-            res: 1.0,
-            input: String::from("1.0 + 0.0"),
-        };
-    }
+    // pub fn new() -> Interpreter {
+    //     return Interpreter {
+    //         res: 1.0,
+    //         input: String::from("1.0 + 0.0"),
+    //     };
+    // }
 
     pub fn input(&mut self, _input: &str) -> Result<Option<f32>, String> {
-        if _input == "A" {
-            return Ok(Some(1.0));
+        let tmp = _input.split_whitespace().collect::<Vec<&str>>();
+        if !tmp
+            .iter()
+            .any(|x| x == &"+" || x == &"-" || x == &"*" || x == &"/")
+        {
+            return Err(_input.to_string());
         }
-        return Err(_input.to_string())
+        let mut res = 0.0;
+        let mut operator: Option<&str> = None;
+        for letter in tmp {
+            println!("{}", letter);
+            if letter.parse::<f32>().is_ok() && operator != None {
+                match operator {
+                    Some("+") => res = res + letter.parse::<f32>().unwrap(),
+                    Some("-") => res = res - letter.parse::<f32>().unwrap(),
+                    Some("*") => res = res * letter.parse::<f32>().unwrap(),
+                    Some("/") => res = res / letter.parse::<f32>().unwrap(),
+                    _ => return Err(_input.to_string())
+                };
+            } else if letter.parse::<f32>().is_ok() {
+                res = letter.parse::<f32>().unwrap();
+            } else {
+                operator = Some(letter);
+            }
+        }
+        // let mut res: f32;
+        // for x in tmp {
+
+        // }
+        // let response = tmp.iter().map(|letter: &str| {
+        //     let mut res: f32;
+            // let a = match letter {
+            //     "+" => 1.0
+            //     _ => res = letter.parse::<f32>().unwrap(),
+            // };
+        //     return res;
+        // });
+        // let operator = tmp.iter().filter(|&&x| {
+        //     x == "+" ||  x == "-" ||  x == "*" ||  x == "/"
+        // });
+
+        // println!("{:?}", operator);
+        // if _input == "A" {
+        return Ok(Some(res));
+        // }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,16 @@ fn main() {
         _ => { println!("None") }
     }
     
-    let mut i = Interpreter::new();
-    println!("{}", i);
-    println!("{:?}", i.input("BA"));
+    let mut i = Interpreter {};
+    println!("{:?}", i.input("1 + 1"));
+}
+
+#[test]
+fn basic_arithmetic() {
+    let mut i = Interpreter {};
+    assert_eq!(i.input("1 + 1"), Ok(Some(2.0)));
+    assert_eq!(i.input("2 - 1"), Ok(Some(1.0)));
+    assert_eq!(i.input("2 * 3"), Ok(Some(6.0)));
+    assert_eq!(i.input("8 / 4"), Ok(Some(2.0)));
+    // assert_eq!(i.input("7 % 4"), Ok(Some(3.0)));
 }
