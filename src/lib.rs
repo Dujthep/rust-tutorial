a5807475e67470311b8baf4b8a6da2110800d720
dujthep-odds <dujthep@odds.team>
2022-04-12 01:49:49 +0700

    add check point.

--- src/lib.rs
+++ src/lib.rs
@@ -6,72 +6,52 @@ impl Dog {
     }
 }
 
-pub struct Interpreter {
-    // res: f32,
-    // input: String,
-}
-
-// impl std::fmt::Display for Interpreter {
-//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-//         write!(f, "(response is : {}, input is : {})", self.res, self.input)
-//     }
-// }
+pub struct Interpreter;
 
 impl Interpreter {
-    // pub fn new() -> Interpreter {
-    //     return Interpreter {
-    //         res: 1.0,
-    //         input: String::from("1.0 + 0.0"),
-    //     };
-    // }
 
     pub fn input(&mut self, _input: &str) -> Result<Option<f32>, String> {
         let tmp = _input.split_whitespace().collect::<Vec<&str>>();
         if !tmp
             .iter()
-            .any(|x| x == &"+" || x == &"-" || x == &"*" || x == &"/")
+            .any(|x| x == &"+" || x == &"-" || x == &"*" || x == &"/" || x == &"%")
         {
             return Err(_input.to_string());
         }
         let mut res = 0.0;
-        let mut operator: Option<&str> = None;
-        for letter in tmp {
-            println!("{}", letter);
-            if letter.parse::<f32>().is_ok() && operator != None {
-                match operator {
-                    Some("+") => res = res + letter.parse::<f32>().unwrap(),
-                    Some("-") => res = res - letter.parse::<f32>().unwrap(),
-                    Some("*") => res = res * letter.parse::<f32>().unwrap(),
-                    Some("/") => res = res / letter.parse::<f32>().unwrap(),
-                    _ => return Err(_input.to_string())
-                };
-            } else if letter.parse::<f32>().is_ok() {
-                res = letter.parse::<f32>().unwrap();
-            } else {
-                operator = Some(letter);
-            }
-        }
-        // let mut res: f32;
-        // for x in tmp {
 
+        operator("+", 0.0, 0.0);
+
+        // let mut operator: Option<&str> = None;
+        // for letter in tmp {
+        //     if letter.parse::<f32>().is_ok() && operator != None {
+        //         match operator {
+        //             Some("+") => res = res + letter.parse::<f32>().unwrap(),
+        //             Some("-") => res = res - letter.parse::<f32>().unwrap(),
+        //             Some("*") => res = res * letter.parse::<f32>().unwrap(),
+        //             Some("/") => res = res / letter.parse::<f32>().unwrap(),
+        //             Some("%") => res = res % letter.parse::<f32>().unwrap(),
+        //             _ => return Err(_input.to_string())
+        //         };
+        //     } else if letter.parse::<f32>().is_ok() {
+        //         res = letter.parse::<f32>().unwrap();
+        //     } else {
+        //         operator = Some(letter);
+        //     }
         // }
-        // let response = tmp.iter().map(|letter: &str| {
-        //     let mut res: f32;
-            // let a = match letter {
-            //     "+" => 1.0
-            //     _ => res = letter.parse::<f32>().unwrap(),
-            // };
-        //     return res;
-        // });
-        // let operator = tmp.iter().filter(|&&x| {
-        //     x == "+" ||  x == "-" ||  x == "*" ||  x == "/"
-        // });
-
-        // println!("{:?}", operator);
-        // if _input == "A" {
         return Ok(Some(res));
-        // }
     }
+}
+
+fn operator(operator: &str, left: f32, right: f32) -> &str {
+    return match operator {
+        "+" => "",
+        "-" => "",
+        "*" => "",
+        "/" => "",
+        "%" => "",
+        _ => operator,
+    };
 }
 
 pub enum FooBar {
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
 use tutorial::Interpreter;
-use tutorial::FooBar;
-use tutorial::Dog;
+// use tutorial::FooBar;
 
 fn main() {
-    let dog = Dog;
-    dog.bark();
-
-    let foobar: Option<FooBar> = Some (FooBar::Bar);
-    match foobar {
-        Some(FooBar::Foo) => { println!("Foo") }
-        Some(FooBar::Bar) => { println!("Bar") }
-        _ => { println!("None") }
-    }
+    // let foobar: Option<FooBar> = Some (FooBar::Bar);
+    // match foobar {
+    //     Some(FooBar::Foo) => { println!("Foo") }
+    //     Some(FooBar::Bar) => { println!("Bar") }
+    //     _ => { println!("None") }
+    // }
     
     let mut i = Interpreter {};
-    println!("{:?}", i.input("1 + 1"));
+    println!("{:?}", i.input("1 + 1 * 2"));
 }
 
 #[test]
@@ -24,5 +20,6 @@ fn basic_arithmetic() {
     assert_eq!(i.input("2 - 1"), Ok(Some(1.0)));
     assert_eq!(i.input("2 * 3"), Ok(Some(6.0)));
     assert_eq!(i.input("8 / 4"), Ok(Some(2.0)));
-    // assert_eq!(i.input("7 % 4"), Ok(Some(3.0)));
+    assert_eq!(i.input("7 % 4"), Ok(Some(3.0)));
+    assert_eq!(i.input("1 + 1 * 2"), Ok(Some(3.0)));
 }
